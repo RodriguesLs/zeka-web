363f6aa177a51696a7fc1436f322d1e87b1720ca
Rodrigues <fr.costa95@gmail.com>
2023-02-10 11:08:19 -0300

    feat(pages): use key to try pagarme

--- src/services/apiPagarme.ts
+++ src/services/apiPagarme.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 const api = axios.create({
   baseURL: import.meta.env.VITE_PAGARME_API_URL,
   headers: {
-    Authorization: 'Basic c2tfV2FubEU5ODNVNHM3ZVJnNjo='
+    Authorization: 'Basic c2tfdGVzdF9SOUpEejBuY1hJUmVQeXFnOg=='
   }
 });
 
